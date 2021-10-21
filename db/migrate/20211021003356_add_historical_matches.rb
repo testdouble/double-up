98b0ed6019811896b2f1a88dde8d607ffa456664
Kenneth Bogner <kenneth.bogner@testdouble.com>
2021-10-21 15:46:43 -0400

    add readme & first migration

---
+++ db/migrate/20211021003356_add_historical_matches.rb
@@ -0,0 +1,11 @@
+class AddHistoricalMatches < ActiveRecord::Migration[6.1]
+  def change
+    create_table :historical_matches do |t|
+      t.string :matches, array: true, default: []
+      t.string :grouping
+      t.date :matched_on
+
+      t.timestamps
+    end
+  end
+end
