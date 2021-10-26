8eb422eb169e886112c9e1deb4090bbce45e13c7
Kenneth Bogner <kenneth.bogner@testdouble.com>
2021-10-26 16:25:28 -0400

    fix matchmaking defaults bug

--- config/initializers/matchmaking.rb
+++ config/initializers/matchmaking.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   matchmaking_config = config_for(:matchmaking).then do |cfg|
     cfg_json = cfg.map { |group, group_config|
-      [group, group_config.merge(group_defaults)]
+      [group, group_config.reverse_merge(group_defaults)]
     }.to_h.to_json
 
     JSON.parse(cfg_json, object_class: OpenStruct)
