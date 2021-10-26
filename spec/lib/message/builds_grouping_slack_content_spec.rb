941ac8e0d83fcaff0118856c3add002b30dce42c
Kenneth Bogner <kenneth.bogner@testdouble.com>
2021-10-26 02:14:06 -0400

    add content service for message

---
+++ spec/lib/message/builds_grouping_slack_content_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe Message::BuildsGroupingSlackContent do
+  let(:subject) { Message::BuildsGroupingSlackContent.new }
+
+  it "renders message as blocks with 2 members" do
+    content = subject.render(
+      grouping: "test",
+      members: ["USER_ID_1", "USER_ID_2"],
+      channel_name: "group-test"
+    )
+
+    expect(content).to eq([
+      {type: "section", text: {type: "mrkdwn", text: <<~MSG.chomp}}
+        :wave: Hi <@USER_ID_1> and <@USER_ID_2>! You've been matched up for test from #group-test! Find a time to meet and have fun!
+      MSG
+    ])
+  end
+
+  it "renders message as blocks with 3 members" do
+    content = subject.render(
+      grouping: "test",
+      members: ["USER_ID_1", "USER_ID_2", "USER_ID_3"],
+      channel_name: "group-test"
+    )
+
+    expect(content).to eq([
+      {type: "section", text: {type: "mrkdwn", text: <<~MSG.chomp}}
+        :wave: Hi <@USER_ID_1>, <@USER_ID_2> and <@USER_ID_3>! You've been matched up for test from #group-test! Find a time to meet and have fun!
+      MSG
+    ])
+  end
+end
