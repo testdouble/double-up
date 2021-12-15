33d66f00ff83cc9640a6004a8f88234ba43fad61
Cliff Pruitt <cliff.pruitt@cliffpruitt.com>
2021-12-15 14:58:12 -0500

    Allow listing inactive configured channels

--- app/controllers/chatops/slack_slash_command_list_controller.rb
+++ app/controllers/chatops/slack_slash_command_list_controller.rb
@@ -4,7 +4,7 @@ module Chatops
       config = Matchmaking.config
       message = config.to_h.keys.sort.each_with_object([]) do |key, result|
         grouping = config[key]
-        next unless grouping.active
+        next unless include_grouping_in_list?(grouping)
         result << "*#{key.to_s.titleize}*: Meets #{grouping.schedule} in groups of #{grouping.size} (Join: ##{grouping.channel})"
       end.join("\n")
 
@@ -13,6 +13,12 @@ module Chatops
       else
         render plain: "Sorry! There are no configured channels for groupings."
       end
+    end
+
+    private
+
+    def include_grouping_in_list?(grouping)
+      grouping.active || command_params.key?(:all)
     end
   end
 end
--- spec/requests/chatops/slack_slash_command_list_controller_spec.rb
+++ spec/requests/chatops/slack_slash_command_list_controller_spec.rb
@@ -88,6 +88,62 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
     expect(response.body).to eq(expected_response)
   end
 
+  scenario "responds with list of active and inactive channels with --all" do
+    allow(Matchmaking).to receive(:config).and_return(OpenStruct.new({
+      active_one: OpenStruct.new({
+        active: true,
+        schedule: "weekly",
+        size: 2,
+        channel: "active-1"
+      }),
+      not_active_one: OpenStruct.new({
+        active: false,
+        schedule: "weekly",
+        size: 2,
+        channel: "not-active-1"
+      }),
+      active_two: OpenStruct.new({
+        active: true,
+        schedule: "weekly",
+        size: 2,
+        channel: "active-2"
+      })
+    }))
+
+    signed = signed_request_body(params: {
+      "team_id" => "T02PF6RHYSY",
+      "team_domain" => "testdouble-hq",
+      "channel_id" => "C02NYBB3VPH",
+      "channel_name" => "some-channel",
+      "user_id" => "U02PRHH0XEV",
+      "user_name" => "cliff.pruitt",
+      "command" => "/doubleup",
+      "text" => "list --all",
+      "api_app_id" => "A02PD0DUE03",
+      "is_enterprise_install" => "false",
+      "response_url" =>
+        "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
+      "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
+    })
+
+    request_headers = {
+      "X-Slack-Signature" => signed.signature,
+      "X-Slack-Request-Timestamp" => signed.timestamp
+    }
+
+    post "/command/doubleup",
+      params: signed.body,
+      headers: request_headers
+
+    expect(response).to have_http_status(:ok)
+    expected_response = <<~MSG.chomp
+      *Active One*: Meets weekly in groups of 2 (Join: #active-1)
+      *Active Two*: Meets weekly in groups of 2 (Join: #active-2)
+      *Not Active One*: Meets weekly in groups of 2 (Join: #not-active-1)
+    MSG
+    expect(response.body).to eq(expected_response)
+  end
+
   private
 
   def signed_request_body(params: {})
