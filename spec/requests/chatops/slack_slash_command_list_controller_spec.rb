cc010d1a4517f76d17c12b84fe57899d7d3d0797
Cliff Pruitt <cliff.pruitt@cliffpruitt.com>
2021-12-15 14:44:34 -0500

    Refactor tests with better signed params handling

--- spec/requests/chatops/slack_slash_command_list_controller_spec.rb
+++ spec/requests/chatops/slack_slash_command_list_controller_spec.rb
@@ -4,20 +4,7 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
   scenario "responds with default message when no channels configured" do
     allow(Matchmaking).to receive(:config).and_return({})
 
-    slack_signing_secret = Slack::Events.config.signing_secret
-    timestamp = Time.zone.now.to_i
-    request_body = "token=#{slack_signing_secret}&team_id=T02PF6RHYSY&team_domain=testdouble-hq&channel_id=C02NYBB3VPH&channel_name=some-channel&user_id=U02PRHH0XEV&user_name=cliff.pruitt&command=%2Fdoubleup&text=list&api_app_id=A02PD0DUE03&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02PF6RHYSY%2F2823421496992%2F0WC0HfWeGJpHetxmF8yUmawo&trigger_id=2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
-    data = ["v0", timestamp, request_body].join(":")
-    mac = OpenSSL::HMAC.hexdigest("SHA256", slack_signing_secret, data)
-    signature = "v0=#{mac}"
-
-    request_headers = {
-      "X-Slack-Signature" => signature,
-      "X-Slack-Request-Timestamp" => timestamp
-    }
-
-    request_params = {
-      "token" => slack_signing_secret,
+    signed = signed_request_body(params: {
       "team_id" => "T02PF6RHYSY",
       "team_domain" => "testdouble-hq",
       "channel_id" => "C02NYBB3VPH",
@@ -31,10 +18,15 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
       "response_url" =>
        "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
       "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
+    })
+
+    request_headers = {
+      "X-Slack-Signature" => signed.signature,
+      "X-Slack-Request-Timestamp" => signed.timestamp
     }
 
     post "/command/doubleup",
-      params: request_params,
+      params: signed.body,
       headers: request_headers
 
     expect(response).to have_http_status(:ok)
@@ -63,20 +55,7 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
       })
     }))
 
-    slack_signing_secret = Slack::Events.config.signing_secret
-    timestamp = Time.zone.now.to_i
-    request_body = "token=#{slack_signing_secret}&team_id=T02PF6RHYSY&team_domain=testdouble-hq&channel_id=C02NYBB3VPH&channel_name=some-channel&user_id=U02PRHH0XEV&user_name=cliff.pruitt&command=%2Fdoubleup&text=list&api_app_id=A02PD0DUE03&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02PF6RHYSY%2F2823421496992%2F0WC0HfWeGJpHetxmF8yUmawo&trigger_id=2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
-    data = ["v0", timestamp, request_body].join(":")
-    mac = OpenSSL::HMAC.hexdigest("SHA256", slack_signing_secret, data)
-    signature = "v0=#{mac}"
-
-    request_headers = {
-      "X-Slack-Signature" => signature,
-      "X-Slack-Request-Timestamp" => timestamp
-    }
-
-    request_params = {
-      "token" => slack_signing_secret,
+    signed = signed_request_body(params: {
       "team_id" => "T02PF6RHYSY",
       "team_domain" => "testdouble-hq",
       "channel_id" => "C02NYBB3VPH",
@@ -88,12 +67,17 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
       "api_app_id" => "A02PD0DUE03",
       "is_enterprise_install" => "false",
       "response_url" =>
-       "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
+        "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
       "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
+    })
+
+    request_headers = {
+      "X-Slack-Signature" => signed.signature,
+      "X-Slack-Request-Timestamp" => signed.timestamp
     }
 
     post "/command/doubleup",
-      params: request_params,
+      params: signed.body,
       headers: request_headers
 
     expect(response).to have_http_status(:ok)
@@ -102,5 +86,20 @@ RSpec.describe "SlackSlashCommandListController", type: :request do
       *Active Two*: Meets weekly in groups of 2 (Join: #active-2)
     MSG
     expect(response.body).to eq(expected_response)
+  end
+
+  private
+
+  def signed_request_body(params: {})
+    slack_signing_secret = Slack::Events.config.signing_secret
+    timestamp = Time.zone.now.to_i
+    request_body = params.to_param
+    data = ["v0", timestamp, request_body].join(":")
+    mac = OpenSSL::HMAC.hexdigest("SHA256", slack_signing_secret, data)
+    OpenStruct.new({
+      signature: "v0=#{mac}",
+      timestamp: timestamp,
+      body: request_body
+    })
   end
 end
