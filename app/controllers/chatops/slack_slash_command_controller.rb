eb4175c324ac8150fc241e232ecc10e09f48a625
Lewis Sparlin <hello@lewismsparlin.com>
2022-01-20 14:47:48 -0600

    WIP - pairing on slack bot command and response with @jongirard

--- app/controllers/chatops/slack_slash_command_controller.rb
+++ app/controllers/chatops/slack_slash_command_controller.rb
@@ -1,7 +1,28 @@
 module Chatops
   class SlackSlashCommandController < ::ApplicationChatopsController
     def handle
-      render plain: "pong"
+
+      # slack_request = Slack::Events::Request.new(request)
+      text_params = params["text"]
+
+      case text_params
+      when "available"
+        "returning available"
+      when "unavailable"
+        "returning unavailable"
+      else
+        render plain: "pong"
+      end
+    end
+
+    private
+
+    def create_availability(slack_channel:, slack_user_id:)
+      GroupingMemberAvailability.create(
+
+        member_id: slack_user_id,
+        availability: :unavailable
+      )
     end
   end
 end
