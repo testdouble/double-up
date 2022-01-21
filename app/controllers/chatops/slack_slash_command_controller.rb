7a07920dd3986b45d6c21250c5e58a5acba67a76
Lewis Sparlin <hello@lewismsparlin.com>
2022-01-21 11:24:07 -0600

    Add command to update member availability, call it from slash command controller, specs for both
    Co-authored-by: Jon Girard <jon.girard@testdouble.com>

--- app/controllers/chatops/slack_slash_command_controller.rb
+++ app/controllers/chatops/slack_slash_command_controller.rb
@@ -1,28 +1,19 @@
 module Chatops
   class SlackSlashCommandController < ::ApplicationChatopsController
     def handle
-
-      # slack_request = Slack::Events::Request.new(request)
       text_params = params["text"]
 
       case text_params
-      when "available"
-        "returning available"
-      when "unavailable"
-        "returning unavailable"
+      when "available", "unavailable"
+        render plain: "Success"
+        Matchmaking::UpdatesParticipantAvailability.new.call(
+          slack_channel: params["channel_name"],
+          member_id: params["user_id"],
+          availability: text_params
+        )
       else
         render plain: "pong"
       end
-    end
-
-    private
-
-    def create_availability(slack_channel:, slack_user_id:)
-      GroupingMemberAvailability.create(
-
-        member_id: slack_user_id,
-        availability: :unavailable
-      )
     end
   end
 end
--- spec/requests/chatops/slack_slash_command_controller_spec.rb
+++ spec/requests/chatops/slack_slash_command_controller_spec.rb
@@ -1,70 +1,97 @@
 require "rails_helper"
 
 RSpec.describe "SlackSlashCommandController", type: :request do
-  let(:kwargs) { "ping" }
-  let(:request_params) {
-    request_params = {
-      "token" => slack_signing_secret,
-      "team_id" => "T02PF6RHYSY",
-      "team_domain" => "testdouble-hq",
-      "channel_id" => "C02NYBB3VPH",
-      "channel_name" => "some-channel",
-      "user_id" => "U02PRHH0XEV",
-      "user_name" => "cliff.pruitt",
-      "command" => "/doubleup",
-      "text" => **kwargs,
-      "api_app_id" => "A02PD0DUE03",
-      "is_enterprise_install" => "false",
-      "response_url" =>
-       "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
-      "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
-    }
-  }
-
+  let(:text) { "anything" }
+  let(:channel) { "rotating-test-channel" }
   let(:slack_signing_secret) { Slack::Events.config.signing_secret }
   let(:timestamp) { Time.zone.now.to_i }
-  let(:request_body) { "token=#{slack_signing_secret}&team_id=T02PF6RHYSY&team_domain=testdouble-hq&channel_id=C02NYBB3VPH&channel_name=some-channel&user_id=U02PRHH0XEV&user_name=cliff.pruitt&command=%2Fdoubleup&text=ping&api_app_id=A02PD0DUE03&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02PF6RHYSY%2F2823421496992%2F0WC0HfWeGJpHetxmF8yUmawo&trigger_id=2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e" }
+  let(:request_body) { "token=#{slack_signing_secret}&team_id=T02PF6RHYSY&team_domain=testdouble-hq&channel_id=C02NYBB3VPH&channel_name=#{channel}&user_id=U02PRHH0XEV&user_name=cliff.pruitt&command=%2Fdoubleup&text=#{text}&api_app_id=A02PD0DUE03&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02PF6RHYSY%2F2823421496992%2F0WC0HfWeGJpHetxmF8yUmawo&trigger_id=2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e" }
   let(:data) { ["v0", timestamp, request_body].join(":") }
   let(:mac) { OpenSSL::HMAC.hexdigest("SHA256", slack_signing_secret, data) }
   let(:signature) { "v0=#{mac}" }
 
-  
-
-  fdescribe "responds to ping with pong" do
-    # slack_signing_secret = Slack::Events.config.signing_secret
-    # timestamp = Time.zone.now.to_i
-    # request_body = "token=#{slack_signing_secret}&team_id=T02PF6RHYSY&team_domain=testdouble-hq&channel_id=C02NYBB3VPH&channel_name=some-channel&user_id=U02PRHH0XEV&user_name=cliff.pruitt&command=%2Fdoubleup&text=ping&api_app_id=A02PD0DUE03&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT02PF6RHYSY%2F2823421496992%2F0WC0HfWeGJpHetxmF8yUmawo&trigger_id=2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
-    # data = ["v0", timestamp, request_body].join(":")
-    # mac = OpenSSL::HMAC.hexdigest("SHA256", slack_signing_secret, data)
-    # signature = "v0=#{mac}"
-
-    request_headers = {
+  let(:request_headers) do
+    {
       "X-Slack-Signature" => signature,
       "X-Slack-Request-Timestamp" => timestamp
     }
+  end
 
-    # request_params = {
-    #   "token" => slack_signing_secret,
-    #   "team_id" => "T02PF6RHYSY",
-    #   "team_domain" => "testdouble-hq",
-    #   "channel_id" => "C02NYBB3VPH",
-    #   "channel_name" => "some-channel",
-    #   "user_id" => "U02PRHH0XEV",
-    #   "user_name" => "cliff.pruitt",
-    #   "command" => "/doubleup",
-    #   "text" => "ping",
-    #   "api_app_id" => "A02PD0DUE03",
-    #   "is_enterprise_install" => "false",
-    #   "response_url" =>
-    #    "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
-    #   "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
-    # }
+  let(:request_params) do
+    {
+      "token" => slack_signing_secret,
+      "team_id" => "T02PF6RHYSY",
+      "team_domain" => "testdouble-hq",
+      "channel_id" => "C02NYBB3VPH",
+      "channel_name" => channel,
+      "user_id" => "U02PRHH0XEV",
+      "user_name" => "cliff.pruitt",
+      "command" => "/doubleup",
+      "text" => text,
+      "api_app_id" => "A02PD0DUE03",
+      "is_enterprise_install" => "false",
+      "response_url" =>
+        "https://hooks.slack.com/commands/T02PF6RHYSY/2823421496992/0WC0HfWeGJpHetxmF8yUmawo",
+      "trigger_id" => "2801968477828.2797229610916.883001cf5008d6d02fd58a3cf70f449e"
+    }
+  end
 
-    subject { post "/command/doubleup", params: request_params, headers: request_headers }
+  before(:example) do
+    @updates_participant_availability = double(Matchmaking::UpdatesParticipantAvailability)
 
-    it "runs the command" do
-      subject
-      # binding.pry
+    allow(Matchmaking::UpdatesParticipantAvailability).to receive(:new) { @updates_participant_availability }
+  end
+
+  context "user asks to be unavailable" do
+    let(:text) { "unavailable" }
+
+    scenario "responds to `/doubleup unavailable`" do
+      expect(@updates_participant_availability).to receive(:call)
+
+      post "/command/doubleup",
+        params: request_params,
+        headers: request_headers
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq("Success")
+    end
+  end
+
+  context "user asks to be available" do
+    let(:text) { "available" }
+
+    scenario "responds to available when user is already available" do
+      expect(@updates_participant_availability).to receive(:call)
+
+      post "/command/doubleup",
+        params: request_params,
+        headers: request_headers
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq("Success")
+    end
+
+    scenario "responds to available when user is unavailable" do
+      expect(@updates_participant_availability).to receive(:call)
+
+      post "/command/doubleup",
+        params: request_params,
+        headers: request_headers
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq("Success")
+    end
+  end
+
+  context "any other text command is given" do
+    let(:text) { "ping" }
+
+    scenario "responds to anything with pong" do
+      expect(@updates_participant_availability).not_to receive(:call)
+
+      post "/command/doubleup",
+        params: request_params,
+        headers: request_headers
 
       expect(response).to have_http_status(:ok)
       expect(response.body).to eq("pong")
