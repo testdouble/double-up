0411be169c991651970b9d98ff16d22c908ef242
Lewis Sparlin <lsparlin@users.noreply.github.com>
2022-04-04 14:05:23 -0500

    resolve #34 update references to brunch bot and remove some outdated-seeming README instructions (#37)

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_mailer/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RotatingBrunch
+module DoubleUp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
