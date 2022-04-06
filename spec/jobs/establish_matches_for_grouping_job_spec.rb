334b71504dab9f9335a16fa329e0b8cc2f0c6a0c
Kenneth Bogner <kenneth.bogner@testdouble.com>
2022-04-06 18:28:58 -0400

    remove notifications from matchmaking job

--- spec/jobs/establish_matches_for_grouping_job_spec.rb
+++ spec/jobs/establish_matches_for_grouping_job_spec.rb
@@ -9,11 +9,9 @@ RSpec.describe EstablishMatchesForGroupingJob do
 
     @loads_slack_channels = double(Slack::LoadsSlackChannels)
     @loads_slack_channel_members = double(Slack::LoadsSlackChannelMembers)
-    @notifies_grouping_members = double(NotifiesGroupingMembers)
 
     allow(Slack::LoadsSlackChannels).to receive(:new) { @loads_slack_channels }
     allow(Slack::LoadsSlackChannelMembers).to receive(:new) { @loads_slack_channel_members }
-    allow(NotifiesGroupingMembers).to receive(:new) { @notifies_grouping_members }
   end
 
   it "raises error when grouping has no associated config" do
@@ -30,7 +28,7 @@ RSpec.describe EstablishMatchesForGroupingJob do
     }.to raise_error("No configured channel for grouping 'test'").and(change(HistoricalMatch, :count).by(0))
   end
 
-  it "matches members of a specific channel and notifies each match" do
+  it "matches members of a specific channel and records the match" do
     expect(@loads_slack_channels).to receive(:call).with(types: "public_channel") {
       [
         Slack::Messages::Message.new(id: "CHANNEL_ID_1", name_normalized: "general"),
@@ -41,13 +39,9 @@ RSpec.describe EstablishMatchesForGroupingJob do
     expect(@loads_slack_channel_members).to receive(:call).with(channel: "CHANNEL_ID_2") {
       ["USER_ID_1", "USER_ID_2", "USER_ID_3", "USER_ID_4", "USER_ID_5"]
     }
-    expect(@notifies_grouping_members).to receive(:call)
-      .with(grouping: "test", members: ["USER_ID_2", "USER_ID_3", "USER_ID_4"], channel_name: "group-test")
-    expect(@notifies_grouping_members).to receive(:call)
-      .with(grouping: "test", members: ["USER_ID_1", "USER_ID_5"], channel_name: "group-test")
 
     expect {
       EstablishMatchesForGroupingJob.new(config: config).perform(grouping: "test")
-    }.to change(HistoricalMatch, :count).by(2)
+    }.to change(HistoricalMatch, :count).by(2).and(change(PendingNotification, :count).by(4))
   end
 end
