8e8518da0b4bdb00ef82853b2f7588bc22919863
Kenneth Bogner <kenneth.bogner@testdouble.com>
2022-04-08 11:17:16 -0400

    refactor BuildsGroupingSlackMessage to take historical match

--- spec/lib/slack/builds_grouping_slack_message_spec.rb
+++ spec/lib/slack/builds_grouping_slack_message_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Slack::BuildsGroupingSlackMessage do
-  let(:subject) { Slack::BuildsGroupingSlackMessage.new }
+  let(:config) {
+    OpenStruct.new(
+      test: OpenStruct.new(channel: "group-test"),
+      rotating_brunch: OpenStruct.new(channel: "rotating-brunch")
+    )
+  }
+  let(:subject) { Slack::BuildsGroupingSlackMessage.new(config: config) }
 
   it "renders message as blocks with 2 members" do
-    content = subject.render(
-      grouping: "test",
-      members: ["USER_ID_1", "USER_ID_2"],
-      channel_name: "group-test"
-    )
+    match = create_historical_match(grouping: "test", members: ["USER_ID_1", "USER_ID_2"])
+
+    content = subject.render(match: match)
 
     expect(content).to eq([
       {type: "section", text: {type: "mrkdwn", text: <<~MSG.chomp}}
@@ -18,11 +22,9 @@ RSpec.describe Slack::BuildsGroupingSlackMessage do
   end
 
   it "renders message as blocks with 3 members" do
-    content = subject.render(
-      grouping: "test",
-      members: ["USER_ID_1", "USER_ID_2", "USER_ID_3"],
-      channel_name: "group-test"
-    )
+    match = create_historical_match(grouping: "test", members: ["USER_ID_1", "USER_ID_2", "USER_ID_3"])
+
+    content = subject.render(match: match)
 
     expect(content).to eq([
       {type: "section", text: {type: "mrkdwn", text: <<~MSG.chomp}}
@@ -32,15 +34,13 @@ RSpec.describe Slack::BuildsGroupingSlackMessage do
   end
 
   it "renders message with humanized grouping name" do
-    content = subject.render(
-      grouping: "rotating_brunch",
-      members: ["USER_ID_1", "USER_ID_2"],
-      channel_name: "group-test"
-    )
+    match = create_historical_match(grouping: "rotating_brunch", members: ["USER_ID_1", "USER_ID_2"])
+
+    content = subject.render(match: match)
 
     expect(content).to eq([
       {type: "section", text: {type: "mrkdwn", text: <<~MSG.chomp}}
-        :wave: Hi <@USER_ID_1> and <@USER_ID_2>! You've been matched up for Rotating Brunch from #group-test! Find a time to meet, and have fun!
+        :wave: Hi <@USER_ID_1> and <@USER_ID_2>! You've been matched up for Rotating Brunch from #rotating-brunch! Find a time to meet, and have fun!
       MSG
     ])
   end
