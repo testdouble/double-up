b70c17391967f60149e72cbf02ec21f18e442c37
Kenneth Bogner <kenneth.bogner@testdouble.com>
2023-01-18 10:59:08 -0500

    Cache user profile information upon retrieval instead of as a separate service

--- app/lib/slack/retrieves_slack_user_info.rb
+++ app/lib/slack/retrieves_slack_user_info.rb
@@ -7,7 +7,21 @@ module Slack
         ClientWrapper.client.users_info(user: user)
       end
 
+      cache_user_info(response&.user, user)
+
       response&.user
+    end
+
+    private
+
+    def cache_user_info(user, slack_user_id)
+      return unless user.present?
+
+      user_profile = SlackUserProfile.find_or_create_by(slack_user_id: slack_user_id)
+      user_profile.update(
+        name: user.profile.real_name,
+        avatar_url: user.profile.image_512
+      )
     end
   end
 end
--- spec/lib/slack/retrieves_slack_user_info_spec.rb
+++ spec/lib/slack/retrieves_slack_user_info_spec.rb
@@ -12,11 +12,31 @@ RSpec.describe Slack::RetrievesSlackUserInfo do
     slack_user = "USER_ID"
 
     expect(@slack_client).to receive(:users_info).with(user: "USER_ID") {
-      Slack::Messages::Message.new(ok: true, user: {id: slack_user})
+      Slack::Messages::Message.new(ok: true, user: {
+        id: slack_user,
+        profile: Slack::Messages::Message.new(real_name: "Luke", image_512: "https://example.com/x/512/512")
+      })
     }
 
     user_info = subject.call(user: slack_user)
 
     expect(user_info.id).to eq(slack_user)
+  end
+
+  it "stores some user profile information to the database" do
+    slack_user = "USER_ID"
+
+    expect(@slack_client).to receive(:users_info).with(user: "USER_ID") {
+      Slack::Messages::Message.new(ok: true, user: {
+        id: slack_user,
+        profile: Slack::Messages::Message.new(real_name: "Luke", image_512: "https://example.com/x/512/512")
+      })
+    }
+
+    expect {
+      subject.call(user: slack_user)
+    }.to change {
+      SlackUserProfile.find_by(slack_user_id: slack_user)
+    }.from(NilClass)
   end
 end
