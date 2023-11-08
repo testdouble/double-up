e699d0b6d6d9ae416e0ea3c084301aeaefafd39c
Ali Ibrahim <aibrahim2k2@gmail.com>
2023-11-08 21:00:45 +0000

    fixup! Add MatchmakingGroups index

--- app/controllers/matchmaking_groups_controller.rb
+++ app/controllers/matchmaking_groups_controller.rb
@@ -1,7 +1,7 @@
 class MatchmakingGroupsController < ApplicationController
   def index
     @groups = CollectsGroups.new.call.to_h
-      .reduce([]) { |acc,(k,v)| acc << v.to_h.merge(name: k) }
+      .reduce([]) { |acc, (k, v)| acc << v.to_h.merge(name: k) }
       .sort_by { |g| [g[:readonly], g[:name]] }
   end
 end
