162ff8855b95bf511d70827f1e3b16e24ca10ac5
Kenneth Bogner <kenneth.bogner@testdouble.com>
2024-02-14 15:59:03 -0500

    Add group_with test helper

--- test/support/config_test_helper.rb
+++ test/support/config_test_helper.rb
@@ -10,4 +10,18 @@ module ConfigTestHelper
 
     OpenStruct.new(normalized)
   end
+
+  def group_with(name:, **hash)
+    raise ArgumentError, "arg must be a Hash" unless hash.is_a?(Hash)
+
+    normalized = hash.transform_keys do |key|
+      next :target_size if key.intern == :size
+      next :is_active if key.intern == :active
+      next :slack_channel_name if key.intern == :channel
+      key
+    end
+    group_defaults = {target_size: 2, is_active: true, schedule: :weekly, slack_channel_name: "test-channel"}
+
+    MatchmakingGroup.new(normalized.reverse_merge(group_defaults).merge(name: name))
+  end
 end
