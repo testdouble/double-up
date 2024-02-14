29feaa7a9a5d5f08e070abd9eb161d2b2a7f10bb
Kenneth Bogner <kenneth.bogner@testdouble.com>
2024-02-14 18:34:53 -0500

    Remove unused test helper

--- test/support/config_test_helper.rb
+++ test/support/config_test_helper.rb
@@ -1,19 +1,5 @@
 module ConfigTestHelper
-  def build_config(hash = {})
-    raise ArgumentError, "arg must be a Hash" unless hash.is_a?(Hash)
-
-    group_defaults = {size: 2, active: true, schedule: :weekly, channel: "test-channel"}
-
-    normalized = hash.map do |group, group_config|
-      [group, OpenStruct.new(group_config.reverse_merge(group_defaults).slice(:size, :active, :schedule, :channel))]
-    end.to_h
-
-    OpenStruct.new(normalized)
-  end
-
   def group_with(name:, **hash)
-    raise ArgumentError, "arg must be a Hash" unless hash.is_a?(Hash)
-
     normalized = hash.transform_keys do |key|
       next :target_size if key.intern == :size
       next :is_active if key.intern == :active
