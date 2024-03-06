2e1da4cd6396a549962e34a954ddf525e54b14f9
Kenneth Bogner <kenneth.bogner@testdouble.com>
2024-03-06 00:43:34 -0500

    Refactor CollectGroups

--- app/services/collect_groups.rb
+++ app/services/collect_groups.rb
@@ -5,23 +5,6 @@ class CollectGroups
 
   def call
     extra_groups = MatchmakingGroup.all
-    readonly_groups + extra_groups
-  end
-
-  private
-
-  def readonly_groups
-    @config.to_h.map do |name, group_config|
-      normalized = group_config.to_h.transform_keys do |key|
-        next :target_size if key.intern == :size
-        next :is_active if key.intern == :active
-        next :slack_channel_name if key.intern == :channel
-        key
-      end
-
-      group = MatchmakingGroup.new(normalized.merge(name: name))
-      group.define_singleton_method(:readonly?) { true }
-      group
-    end
+    @config.groups + extra_groups
   end
 end
--- test/support/config_test_helper.rb
+++ test/support/config_test_helper.rb
@@ -1,14 +1,23 @@
 module ConfigTestHelper
   def group_with(name:, **hash)
-    normalized = hash.transform_keys do |key|
-      next :target_size if key.intern == :size
-      next :is_active if key.intern == :active
-      next :slack_channel_name if key.intern == :channel
-      key
+    group_defaults = {
+      target_size: 2,
+      is_active: true,
+      schedule: :weekly,
+      slack_channel_name: "test-channel"
+    }
+    attrs = hash.reverse_merge(group_defaults).merge(name: name)
+    MatchmakingGroup.new(attrs).tap do |group|
+      group.define_singleton_method(:readonly?) { true }
     end
-    group_defaults = {target_size: 2, is_active: true, schedule: :weekly, slack_channel_name: "test-channel"}
+  end
 
-    MatchmakingGroup.new(normalized.reverse_merge(group_defaults).merge(name: name))
+  def stub_matchmaking_config(groups, &block)
+    config = Mocktail.of(Matchmaking::Config)
+    Rails.application.config.x.stub :matchmaking, config do
+      stubs { config.groups }.with { groups }
+      block.call
+    end
   end
 
   def matchmaking_config_of(hash = {})
