85090da37eaa40f7937094c34d6786f0a457aed8
Kenneth Bogner <kenneth.bogner@testdouble.com>
2024-03-29 01:39:20 -0400

    Add quest_protraction message service

---
+++ app/services/slack/build_quest_protraction_message.rb
@@ -0,0 +1,30 @@
+module Slack
+  class BuildQuestProtractionMessage
+    def initialize
+      @humanize_names = Utils::HumanizeNames.new
+    end
+
+    def call(match:)
+      Slack::BlockKit.blocks do |b|
+        b.section do |s|
+          s.mrkdwn(text: saluations_text(match))
+        end
+        b.section do |s|
+          s.mrkdwn(text: I18n.t("slack.message.quest_protraction.body"))
+        end
+        b.actions do |a|
+          a.button(text: I18n.t("slack.message.quest_protraction.buttons.complete"), action_id: "action-complete", value: "quest_complete")
+          a.button(text: I18n.t("slack.message.quest_protraction.buttons.continue"), action_id: "action-continue", value: "quest_continue")
+        end
+      end
+    end
+
+    private
+
+    def saluations_text(match)
+      member_mentions = match.members.map { |m| I18n.t("slack.message.mention", slack_user_id: m) }
+      humanized_mentions = @humanize_names.call(member_mentions)
+      I18n.t("slack.message.quest_protraction.salutation", mentions: humanized_mentions)
+    end
+  end
+end
---
+++ test/services/slack/build_quest_protraction_message_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+module Slack
+  class BuildQuestProtractionMessageTest < ActiveSupport::TestCase
+    setup do
+      @subject = BuildQuestProtractionMessage.new
+    end
+
+    test "renders message as blocks" do
+      match = create_historical_match(grouping: "test", members: ["Frodo", "Sam"])
+
+      content = @subject.call(match: match)
+
+      assert_blocks_match [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Hi <@Frodo> and <@Sam>!\n"
+          }
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Are you done with your quest?\n"
+          }
+        },
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Yes!"
+              },
+              action_id: "action-complete",
+              value: "quest_complete"
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "No, we need more time"
+              },
+              action_id: "action-continue",
+              value: "quest_continue"
+            }
+          ]
+        }
+      ], content
+    end
+  end
+end
