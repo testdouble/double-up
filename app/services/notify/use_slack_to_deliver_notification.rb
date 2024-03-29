c7e4ef2ddb65a5cd0404f32487e7d9f0f52f75e4
Kenneth Bogner <kenneth.bogner@testdouble.com>
2024-03-29 02:22:22 -0400

    Handle quest_protraction reason

--- app/services/notify/use_slack_to_deliver_notification.rb
+++ app/services/notify/use_slack_to_deliver_notification.rb
@@ -4,6 +4,7 @@ module Notify
       @opens_slack_conversation = Slack::OpensSlackConversation.new
       @sends_slack_message = Slack::SendsSlackMessage.new
       @build_new_match_message = Slack::BuildNewMatchMessage.new
+      @build_quest_protraction_message = Slack::BuildQuestProtractionMessage.new
     end
 
     def call(notification, group)
@@ -15,8 +16,23 @@ module Notify
 
       @sends_slack_message.call(
         channel: match_conversation,
-        blocks: @build_new_match_message.call(match: match, channel_name: group.slack_channel_name)
+        blocks: build_appropriate_message(notification, group)
       )
+    end
+
+    private
+
+    def build_appropriate_message(notification, group)
+      match = notification.historical_match
+      reason = notification.reason
+      channel_name = group.slack_channel_name
+
+      case reason
+      when "quest_protraction"
+        @build_quest_protraction_message.call(match: match)
+      when "new_match"
+        @build_new_match_message.call(match: match, channel_name: channel_name)
+      end
     end
   end
 end
--- test/services/notify/use_slack_to_deliver_notification_test.rb
+++ test/services/notify/use_slack_to_deliver_notification_test.rb
@@ -13,7 +13,7 @@ module Notify
       @subject = UseSlackToDeliverNotification.new
     end
 
-    test "sends a single group slack message to all members" do
+    test "sends a single group slack message to all members with new_match reason" do
       notification = create_pending_slack_notification
       match = create_historical_match(
         grouping: "test",
@@ -23,6 +23,21 @@ module Notify
 
       stubs { @opens_slack_conversation.call(users: ["USER_ID_1", "USER_ID_2"]) }.with { "MPIM_ID" }
       stubs { @build_new_match_message.call(match: match, channel_name: "test") }.with { [] }
+      stubs { @sends_slack_message.call(channel: "MPIM_ID", blocks: []) }
+
+      @subject.call(notification, @group)
+    end
+
+    test "sends a single group slack message to all members with quest_protraction reason" do
+      notification = create_pending_slack_notification(reason: "quest_protraction")
+      match = create_historical_match(
+        grouping: "test",
+        members: ["USER_ID_1", "USER_ID_2"],
+        pending_notifications: [notification]
+      )
+
+      stubs { @opens_slack_conversation.call(users: ["USER_ID_1", "USER_ID_2"]) }.with { "MPIM_ID" }
+      stubs { @build_quest_protraction_message.call(match: match) }.with { [] }
       stubs { @sends_slack_message.call(channel: "MPIM_ID", blocks: []) }
 
       @subject.call(notification, @group)
@@ -38,6 +53,7 @@ module Notify
 
       stubs { |m| @opens_slack_conversation.call(users: m.any) }.with { raise "Should not be called" }
       stubs { |m| @build_new_match_message.call(match: m.any, channel_name: m.any) }.with { raise "Should not be called" }
+      stubs { |m| @build_quest_protraction_message.call(match: m.any) }.with { raise "Should not be called" }
       stubs { |m| @sends_slack_message.call(channel: m.any, blocks: m.any) }.with { raise "Should not be called" }
 
       @subject.call(notification, @group)
